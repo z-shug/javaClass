8c5c838849b9c89bafba7b7fb7595c69ea04930e
z-shug <83725819+z-shug@users.noreply.github.com>
2022-08-02 16:26:19 -0700

    added

---
+++ HelloWorld/src/main/java/zls/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author Your Name
+ *email: youremail@domain.com
+ *date: current date
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package zls.helloworld; 
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
