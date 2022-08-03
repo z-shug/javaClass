cc888fa49a371c35d39511314295f305806a781b
z-shug <83725819+z-shug@users.noreply.github.com>
2022-08-03 11:52:50 -0700

    added excersise 1

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenburg.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author zshug
+ */
+public class ProjectGutenburg {
+        public static void main(String[] args) {
+            System.out.print("Did you know that in 1440 (or thereabouts),");
+            System.out.println("Johannes Gutenberg invented the printing press?");
+            System.out.println("He started out as a goldsmith!");
+            System.out.print("His invention made it easy to print and");
+            System.out.println("distribute books to anyone who wanted one.");
+            System.out.print("We are like a modern Gutenberg,");
+            System.out.println("printing vast amounts to the waiting console with ease.");
+    }
+    
+}
