610e97d0c512602f32179e3213e2636afcd86764
z-shug <83725819+z-shug@users.noreply.github.com>
2022-08-04 11:42:33 -0700

    updated helloworld

--- HelloWorld/src/main/java/zls/helloworld/Hello.java
+++ HelloWorld/src/main/java/zls/helloworld/Hello.java
@@ -11,5 +11,6 @@ package zls.helloworld;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("My name is Harry!");
     }
 }
